f070fb7aa656dc48ec6a7bf01cba941de1347651
Velcorn <Velcorn@users.noreply.github.com>
2024-07-22 17:12:53 +0200

    Minor text fixes

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-import speedtest
+import subprocess
 import pandas as pd
 import os
 import datetime
+import json
 
 # Path to save speed test results
-file_path = 'speedtest_results.csv'
+file_path = '/home/velcorn/SpeedtestPi/results.csv'
 
 
 # Initialize the speed test
 def run_speedtest():
-    st = speedtest.Speedtest()
-    st.download()
-    st.upload()
-    return st.results.dict()
+    result = subprocess.run(['speedtest.exe', '--format', 'json'], capture_output=True, text=True)
+    return json.loads(result.stdout)
 
 
 # Save results to a CSV file
 def save_to_csv(data, file_path):
     df = pd.DataFrame([data])
-    # Convert down/up to Mb/s, limit to timestamp, ping, download, and upload columns and round to 0 decimal places
+    # Convert down/up to Mb/s, limit to timestamp, ping, jitter, download, upload, and jitter columns and round
     df['download'] = df['download'] / 1e6
     df['upload'] = df['upload'] / 1e6
-    df = df[['timestamp', 'ping', 'download', 'upload']].round(0)
+    df = df[['timestamp', 'ping', 'ping_jitter', 'download', 'download_jitter', 'upload', 'upload_jitter']].round(0)
     if not os.path.isfile(file_path):
         df.to_csv(file_path, index=False)
     else:
@@ -30,10 +29,21 @@ def save_to_csv(data, file_path):
 
 # Main function to run speed test and save results
 def main():
-    results = run_speedtest()
-    # Add datetime rounded to seconds
-    results['timestamp'] = datetime.datetime.now().replace(microsecond=0)
-    save_to_csv(results, file_path)
+    try:
+        results = run_speedtest()
+        results = {
+            'timestamp': datetime.datetime.now().replace(microsecond=0),
+            'ping': results['ping']['latency'],
+            'ping_jitter': results['ping']['jitter'],
+            'download': results['download']['bandwidth'],
+            'download_jitter': results['download']['latency']['jitter'],
+            'upload': results['upload']['bandwidth'],
+            'upload_jitter': results['upload']['latency']['jitter']
+        }
+        save_to_csv(results, file_path)
+        print('Speed test completed successfully.')
+    except Exception as e:
+        print(f'Error running speed test: {e}')
 
 
 if __name__ == '__main__':
